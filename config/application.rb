eb108c81f984dfbc6094ceb41764b92c5e40e49f
r.lane <r.lane@fluidappsdev.com>
2014-09-16 21:41:44 -0700

    adding root project path to load_path

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require 'active_model'
+$LOAD_PATH.unshift(__dir__)
 Dir[__dir__ + '/../app/**/'].each { |d| $LOAD_PATH.unshift(d) }
 require 'base'
 Dir[__dir__+'/../app/**/*.rb'].each { |f| require File.basename(f) }
